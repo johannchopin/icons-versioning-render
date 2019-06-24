c9f46d1cf5ceae4137854554a765deab3d39b73d
Johann Chopin <johannchopin@pm.me>
2019-06-24 16:15:30 +0100

    Add customColor input

--- src/default.js
+++ src/default.js
@@ -9,11 +9,17 @@ function initColorPalette() {
     for (var elem of document.querySelectorAll(".lj-colorBox")) {
         elem.addEventListener("click", updateBgColor);
     }
+
+    document.getElementById("bgColorInput").addEventListener("change", updateBgColorWithCustomColor, false);
 }
 
-
-
 function updateBgColor(event) {
+    document.body.removeAttribute("style");
+
     clickedElmtClass = event.target.className;
     document.body.className = clickedElmtClass.replace("lj-colorBox", "");
+}
+
+function updateBgColorWithCustomColor(event) {
+    document.body.style.backgroundColor = event.target.value;
 }
